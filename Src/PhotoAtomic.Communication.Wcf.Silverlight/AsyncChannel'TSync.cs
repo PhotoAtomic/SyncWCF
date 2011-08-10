25f814fcbf934bf5566030d24b5ca209588de513
Duke360_cp <PhotoAtomic@users.noreply.github.com>
2011-08-10 19:37:42 +0000

    impoved intellisense support: no need to add "," (comma) inorder to see the list of member of the first argument of ExecuteAsync

--- Src/PhotoAtomic.Communication.Wcf.Silverlight/AsyncChannel'TSync.cs
+++ Src/PhotoAtomic.Communication.Wcf.Silverlight/AsyncChannel'TSync.cs
@@ -12,7 +12,7 @@
     /// represent an async channel that could be used to invoke the server
     /// </summary>
     /// <typeparam name="TSync">the sync type on wich the channel is based on</typeparam>
-    public class AsyncChannel<TSync>
+    public partial class AsyncChannel<TSync>
     {
         /// <summary>
         /// dictionary of the begin methods
@@ -74,104 +74,17 @@
             parameterTypes.Add(method.ReturnType);
 
             return Expression.GetDelegateType(parameterTypes.ToArray());
-        }
-
-        /// <summary>
-        /// Executes a synchronous operation defined on the Sync interface in an asynch fashon
-        /// </summary>
-        /// <typeparam name="TOut">the type of result expected from the invokation of the operaton</typeparam>
-        /// <param name="requestInvokation">the request to perform on the channel expressed as Sync</param>
-        /// <param name="responseAction">the action to perform when the reply arrives</param>
-        /// <returns>an IAsyncResult for the asyncronous operation, it could be useful to check when the operation is completed</returns>
-        public IAsyncResult ExecuteAsync<TOut>(Expression<Func<TSync, TOut>> requestInvokation, Action<TOut> responseAction, Action<Exception> onException = null)
-        {
-            var lambda = requestInvokation as LambdaExpression;
-            var method = lambda.Body as MethodCallExpression;
-
-            var argumentsValues = ExtractArgumentValuesList(method);
-            
-            Dispatcher dispatcher = System.Windows.Deployment.Current.Dispatcher;
-
-            AsyncCallback callback = asyncResult =>
-                {
-                    try
-                    {
-                        var parameters = Enumerable.Repeat<object>(null, method.Arguments.Count + 1).ToArray();
-                        parameters[method.Arguments.Count] = asyncResult;   
-                        TOut result = (TOut)endOperations[method.Method.Name].DynamicInvoke(parameters);
-                        dispatcher.BeginInvoke(() =>
-                            {
-                                new Assigner<TSync>().Assign(requestInvokation, parameters);
-                                responseAction(result); 
-                            });
-                    }
-                    catch (Exception ex)
-                    {
-                        if (onException == null) return;
-                        Exception exception = ex.InnerException ?? ex;
-                        dispatcher.BeginInvoke(() => onException(ex));
-                    }
-                };
-            
-            argumentsValues.Add(callback);
-            argumentsValues.Add(null);
-
-            IAsyncResult invokationResult = BeginInvokation(method.Method.Name, argumentsValues);
-
-            return invokationResult;                       
-        }
+        }       
 
         /// <summary>
         /// Executes a synchronous operation defined on the Sync interface in an asynch fashon
         /// </summary>        
         /// <param name="requestInvokation">the request to perform on the channel expressed as Sync</param>
+        /// <param name="asyncState">an user defined object to transmit to the ascyn reply of the operation</param>
         /// <param name="responseAction">the action to perform when the reply arrives</param>
+        /// <param name="onException">invoked when exception occurs</param>
         /// <returns>an IAsyncResult for the asyncronous operation, it could be useful to check when the operation is completed</returns>
-        public IAsyncResult ExecuteAsync(Expression<Action<TSync>> requestInvokation, Action responseAction, Action<Exception> onException = null)
-        {
-            var lambda = requestInvokation as LambdaExpression;
-            var method = lambda.Body as MethodCallExpression;
-
-            var argumentsValues = ExtractArgumentValuesList(method);
-
-            Dispatcher dispatcher = System.Windows.Deployment.Current.Dispatcher;
-
-            AsyncCallback callback = asyncResult =>
-            {
-                try
-                {
-                    var parameters = Enumerable.Repeat<object>(null, method.Arguments.Count + 1).ToArray();
-                    parameters[method.Arguments.Count] = asyncResult;                    
-                    endOperations[method.Method.Name].DynamicInvoke(parameters);
-                    dispatcher.BeginInvoke(()=>
-                        {
-                            new Assigner<TSync>().Assign(requestInvokation, parameters);
-                            responseAction();
-                        });
-                }
-                catch (Exception ex)
-                {
-                    if (onException == null) return;
-                    Exception exception = ex.InnerException ?? ex;
-                    dispatcher.BeginInvoke(() => onException(ex));
-                }
-            };
-
-            argumentsValues.Add(callback);
-            argumentsValues.Add(null);
-
-            IAsyncResult invokationResult = BeginInvokation(method.Method.Name, argumentsValues);
-
-            return invokationResult;
-        }
-
-        /// <summary>
-        /// Executes a synchronous operation defined on the Sync interface in an asynch fashon
-        /// </summary>        
-        /// <param name="requestInvokation">the request to perform on the channel expressed as Sync</param>
-        /// <param name="responseAction">the action to perform when the reply arrives</param>
-        /// <returns>an IAsyncResult for the asyncronous operation, it could be useful to check when the operation is completed</returns>
-        public IAsyncResult ExecuteAsync(Expression<Action<TSync>> requestInvokation, object asyncState, Action<object> responseAction, Action<Exception> onException = null)
+        public IAsyncResult ExecuteAsync(Expression<Action<TSync>> requestInvokation, object asyncState, Action<object> responseAction, Action<Exception, object> onException)
         {
             var lambda = requestInvokation as LambdaExpression;
             var method = lambda.Body as MethodCallExpression;
@@ -198,7 +111,7 @@
                 {
                     if (onException == null) return;
                     Exception exception = ex.InnerException ?? ex;
-                    dispatcher.BeginInvoke(() => onException(ex));
+                    dispatcher.BeginInvoke(() => onException(ex, asyncResult.AsyncState));
                 }
             };
 
@@ -217,8 +130,9 @@
         /// <param name="requestInvokation">the request to perform on the channel expressed as Sync</param>
         /// <param name="asyncState">an user defined object to transmit to the ascyn reply of the operation</param>
         /// <param name="responseAction">the action to perform when the reply arrives</param>
+        /// <param name="onException">invoked when exception occurs</param>
         /// <returns>an IAsyncResult for the asyncronous operation, it could be useful to check when the operation is completed</returns>
-        public IAsyncResult ExecuteAsync<TOut>(Expression<Func<TSync, TOut>> requestInvokation, object asyncState, Action<TOut, object> responseAction, Action<Exception, object> onException = null)
+        public IAsyncResult ExecuteAsync<TOut>(Expression<Func<TSync, TOut>> requestInvokation, object asyncState, Action<TOut, object> responseAction, Action<Exception, object> onException)
         {
             var lambda = requestInvokation as LambdaExpression;
             var method = lambda.Body as MethodCallExpression;
