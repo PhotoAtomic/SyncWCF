8e7ecb39777296e97032fb9bc30450585041cf9e
Duke360_cp <SND\Duke360_cp>
2012-01-20 16:13:20 +0000

    OOopss! version number :)

--- Main/Src/PhotoAtomic.SyncWcf/Properties/AssemblyInfo.cs
+++ Main/Src/PhotoAtomic.SyncWcf/Properties/AssemblyInfo.cs
@@ -9,5 +9,5 @@
 [assembly: AssemblyTrademark("www.photoatomiclab.net")]
 [assembly: AssemblyInformationalVersion("Semantic Versioning")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyVersion("2.0.2.0")]
+[assembly: AssemblyFileVersion("2.0.2.0")]
